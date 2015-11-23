f295e62d8f5d2e1379145ebd87bfd63a101316b8
Josh Yudaken <j@yud.co.za>
2015-11-23 13:08:13 -0800

    Fix unicode handling in callback

--- rdbtools/callbacks.py
+++ rdbtools/callbacks.py
@@ -293,10 +293,11 @@ class ProtocolCallback(RdbCallback):
             self.expireat(key, self.get_expiry_seconds(key))
 
     def emit(self, *args):
-        self._out.write(u"*" + unicode(len(args)) + u"\r\n")
+        self._out.write('*' + str(len(args)) + '\r\n')
         for arg in args:
-            self._out.write(u"$" + unicode(len(unicode(arg))) + u"\r\n")
-            self._out.write(unicode(arg) + u"\r\n")
+            value = str(arg)
+            self._out.write('$' + str(len(value)) + '\r\n')
+            self._out.write(value + '\r\n')
 
     def start_database(self, db_number):
         self.reset()
