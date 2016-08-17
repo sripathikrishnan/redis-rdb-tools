sripathikrishnan/redis-rdb-tools

commit b2e2d5e88f7497cb9651bd52076694f78ae26ef0

Added filtering of the top X largest keys

rdbtools/memprofiler.py
@@ -5,6 +5,8 @@ import json
 from rdbtools.parser import RdbCallback
 from rdbtools.callbacks import encode_key
 
+from heapq import heappush, nlargest, heappop
+
 ZSKIPLIST_MAXLEVEL=32
 ZSKIPLIST_P=0.25
 REDIS_SHARED_INTEGERS = 10000
@@ -68,16 +70,32 @@ class StatsAggregator():
         return json.dumps({"aggregates":self.aggregates, "scatters":self.scatters, "histograms":self.histograms})
         
 class PrintAllKeys():
-    def __init__(self, out, size):
+    def __init__(self, out, size, largest):
         self._size = size
+        self._largest = largest
         self._out = out
         self._out.write("%s,%s,%s,%s,%s,%s,%s\n" % ("database", "type", "key", 
                                                  "size_in_bytes", "encoding", "num_elements", "len_largest_element"))
+
+        if self._largest is not None:
+            self._heap = []
     
     def next_record(self, record) :
-        if self._size is None or record.size >= int(self._size):
-            self._out.write("%d,%s,%s,%d,%s,%d,%d\n" % (record.database, record.type, encode_key(record.key), 
-                                                     record.bytes, record.encoding, record.size, record.len_largest_element))
+        if self._largest is None:
+            if self._size is None or record.bytes >= int(self._size):
+                self._out.write("%d,%s,%s,%d,%s,%d,%d\n" % (record.database, record.type, encode_key(record.key), 
+                                                         record.bytes, record.encoding, record.size, record.len_largest_element))
+        else:
+            heappush(self._heap, (record.bytes, record))
+            self._heap = nlargest(int(self._largest), self._heap)
+
+    def dump_heap(self):
+        if self._largest is not None:
+            self._largest = None
+
+            while self._heap:
+                bytes, record = heappop(self._heap)
+                self.next_record(record)
     
 class MemoryCallback(RdbCallback):
     '''Calculates the memory used if this rdb file were loaded into RAM
@@ -106,8 +124,9 @@ class MemoryCallback(RdbCallback):
         pass
         
     def end_rdb(self):
+        self._stream.dump_heap()
         pass
-       
+        
     def set(self, key, value, expiry, info):
         self._current_encoding = info['encoding']
         size = self.sizeof_string(key) + self.sizeof_string(value) + self.top_level_object_overhead()

rdbtools/cli/rdb.py
@@ -24,6 +24,8 @@ Example : %prog --command json -k "user.*" /var/redis/6379/dump.rdb"""
                     If not specified, all data types will be returned""")
     parser.add_option("-s", "--size", dest="size", default=None,
                   help="Limit memory output to keys greater to or equal to this value (in bytes)")
+    parser.add_option("-l", "--largest", dest="largest", default=None,
+                  help="Limit memory output to only the top N keys (by size)")
     
     (options, args) = parser.parse_args()
     
@@ -59,7 +61,7 @@ Example : %prog --command json -k "user.*" /var/redis/6379/dump.rdb"""
             elif 'json' == options.command:
                 callback = JSONCallback(f)
             elif 'memory' == options.command:
-                reporter = PrintAllKeys(f, options.size)
+                reporter = PrintAllKeys(f, options.size, options.largest)
                 callback = MemoryCallback(reporter, 64)
             elif 'protocol' == options.command:
                 callback = ProtocolCallback(f)
@@ -73,7 +75,7 @@ Example : %prog --command json -k "user.*" /var/redis/6379/dump.rdb"""
         elif 'json' == options.command:
             callback = JSONCallback(sys.stdout)
         elif 'memory' == options.command:
-            reporter = PrintAllKeys(sys.stdout, options.size)
+            reporter = PrintAllKeys(sys.stdout, options.size, options.largest)
             callback = MemoryCallback(reporter, 64)
         elif 'protocol' == options.command:
             callback = ProtocolCallback(sys.stdout)

commit 0a127993e1e092b976a03d9ce8ed10bb8777e502

fix test suite broken by b2e2d5e8 (Added filtering of the top X largest keys)

rdbtools/memprofiler.py
@@ -92,7 +92,7 @@ class PrintAllKeys():
         else:
             heappush(self._heap, (record.bytes, record))
 
-    def dump_heap(self):
+    def end_rdb(self):
         if self._largest is not None:
             self._heap = nlargest(int(self._largest), self._heap)
             self._largest = None
@@ -150,10 +150,13 @@ class MemoryCallback(RdbCallback):
         self._stream.next_record(record)
         record = MemoryRecord(self._dbnum, "dict", None, self.hashtable_overhead(self._db_expires), None, None, None)
         self._stream.next_record(record)
+        if hasattr(self._stream, 'end_database'):
+            self._stream.end_database(db_number)
 
     def end_rdb(self):
         #print('internal fragmentation: %s' % self._total_internal_frag)
-        self._stream.dump_heap()
+        if hasattr(self._stream, 'end_rdb'):
+            self._stream.end_rdb()
 
     def set(self, key, value, expiry, info):
         self._current_encoding = info['encoding']