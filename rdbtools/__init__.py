548b11ec3c81a603f5b321228d07a61a0b940159
Oran Agra <oran@redislabs.com>
2020-06-28 17:21:49 +0300

    bump version 0.1.15

--- rdbtools/__init__.py
+++ rdbtools/__init__.py
@@ -2,7 +2,7 @@ from rdbtools.parser import RdbCallback, RdbParser, DebugCallback
 from rdbtools.callbacks import JSONCallback, DiffCallback, ProtocolCallback, KeyValsOnlyCallback, KeysOnlyCallback
 from rdbtools.memprofiler import MemoryCallback, PrintAllKeys, StatsAggregator, PrintJustKeys
 
-__version__ = '0.1.14'
+__version__ = '0.1.15'
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = [
